88ff65c8c7c4157dfc48257210b6d1afc608a3fa
Jonas Finnemann Jensen <jonasfj@google.com>
2022-11-17 12:23:14 +0100

    Playing around

---
+++ bin/pub_playground.dart
@@ -0,0 +1,5 @@
+import 'package:pub_playground/pub_playground.dart' as pub_playground;
+
+void main(List<String> arguments) {
+  print('Hello world: ${pub_playground.calculate()}!');
+}
---
+++ test/pub_playground_test.dart
@@ -0,0 +1,8 @@
+import 'package:pub_playground/pub_playground.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
